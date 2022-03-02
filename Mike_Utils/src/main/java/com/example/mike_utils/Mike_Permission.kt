a7306e4d92bf8a39b7b0a4b025157b5a7725bbdf
Mac <mac@Macs-MacBook-Pro.local>
2022-03-02 23:20:58 +0100

    revert functions to different files

--- Mike_Utils/src/main/java/com/example/mike_utils/Mike_Permission.kt
+++ Mike_Utils/src/main/java/com/example/mike_utils/Mike_Permission.kt
@@ -1,11 +1,7 @@
 package com.example.mike_utils
 
 import android.content.Context
-import android.content.Intent
 import android.os.Build
-import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
@@ -15,52 +11,26 @@ import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener
 import com.karumi.dexter.listener.single.PermissionListener
 
-/**
- * Have access to log, toast, hide and show view, navigateToActivity, askForPermission.
- * ask for permission takes in the permission from the manifest, context and message if the permission was denied.
- */
-object MikeUtils {
+object MikePermission {
 
-    fun <T> log(value : T){
-        Log.i("INFORMATION", "log: $value")
-    }
-
-    fun toast(context: Context, message : String){
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-    }
-
-    fun View.hideView(){
-        visibility = View.GONE
-    }
-
-    fun View.showView(){
-        visibility = View.VISIBLE
-    }
-
-    inline fun <reified T> navigateToActivity(context: Context, destination: T){
-        val intent = Intent(context, T::class.java)
-        context.startActivity(intent)
-    }
-
-    fun askPermission(permission : String, context: Context, message : String) {
+    fun askPermission(permission : String, context: Context, permissionDeniedMessage : String, action: () -> Unit) {
 
         Dexter.withContext(context)
             .withPermission(permission)
             .withListener(object : PermissionListener {
                 @RequiresApi(Build.VERSION_CODES.R)
                 override fun onPermissionGranted(response: PermissionGrantedResponse) { /* ... */
-
+                    action.invoke()
                 }
 
                 override fun onPermissionDenied(response: PermissionDeniedResponse) { /* ... */
                     DialogOnDeniedPermissionListener.Builder
                         .withContext(context)
                         .withTitle("Permission")
-                        .withMessage(message)
+                        .withMessage(permissionDeniedMessage)
                         .withButtonText(android.R.string.ok)
                         .build()
 
-                    toast(context, "Permission is required to use this feature")
                 }
 
                 override fun onPermissionRationaleShouldBeShown(
@@ -72,4 +42,5 @@ object MikeUtils {
             }).check()
 
     }
+
 }
